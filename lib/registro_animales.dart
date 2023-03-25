09ec3c32243890221b2846e78b0ea59c0cc43959
quimmm25 <dowenpett@gmail.com>
2023-03-25 14:32:42 +0100

    1er commit

--- lib/registro_animales.dart
+++ lib/registro_animales.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:lite_rolling_switch/lite_rolling_switch.dart';
 import 'main.dart';
 
 void main() {
