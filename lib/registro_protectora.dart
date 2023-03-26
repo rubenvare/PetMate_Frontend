c98abe6ba97052be37a8bb552ac50e2ee378583c
Loann-ca <loann.cosano@gmail.com>
2023-03-26 14:10:52 +0200

    Mejoras registro protectora

--- lib/registro_protectora.dart
+++ lib/registro_protectora.dart
@@ -1,41 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'main.dart';
 
 class RegistroProtectora extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: PetMateAppBar(),
+    return MaterialApp(
+    home: Scaffold(
+      backgroundColor: Color(0xFFC4A484),
+      appBar: PetMateAppBar(),
+
       body: Padding(
         padding: EdgeInsets.all(16.0),
-        child: Form(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text("Contacto"),
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: "Número del contacto",
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          child: Form(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Número del contacto",
+                    border: OutlineInputBorder(),
+                    labelStyle: GoogleFonts.quicksand(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700
+                    )
+                  ),
                 ),
-              ),
-              SizedBox(height: 16.0),
-              Text("Ubicación"),
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: "Dirección de la protectora",
+                SizedBox(height: 16.0),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Dirección de la protectora",
+                    border: OutlineInputBorder(),
+                    labelStyle: GoogleFonts.quicksand(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700
+                    )
+                  ),
                 ),
-              ),
-              SizedBox(height: 16.0),
-              Center(
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: Text("Registrarse"),
+                SizedBox(height: 16.0),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () {print("Registro pulsado");},
+                    style: ElevatedButton.styleFrom(backgroundColor: Colors.brown),
+                    child: Text("Registrarse"),
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
+     )
     );
   }
 }
