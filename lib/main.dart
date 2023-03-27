afc0697e1311eb9419e1201e855f5f7b802f846b
rubenvare <rubenvare@github.com>
2023-03-27 23:17:24 +0200

    validacion en cliente y colores corregidos

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,17 @@ void main() {
   runApp(MaterialApp(home: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  InicioSesion createState() => InicioSesion();
+}
+
+class InicioSesion extends State<MyApp> {
+  final formKey = GlobalKey<FormState>();
+  bool errorMailRegExp = false;
+  bool errorMailEmpty = false;
+  bool errorPassword = false;
+  RegExp coincideMail = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,7 +29,7 @@ class MyApp extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.center,
-              children: [
+              children: [ // Agregue espacio en blanco sobre el texto
                 Text(
                   "¡BIENVENIDO!",
                   style: GoogleFonts.quicksand(
@@ -29,82 +39,139 @@ class MyApp extends StatelessWidget {
                     letterSpacing: 2.0,
                   ),
                 ),
-                SizedBox(height: 70),
-                Container(
-                  width: 350,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      TextField(
-                        obscureText: true,
-                        decoration: InputDecoration(
-                            border: OutlineInputBorder(),
+                SizedBox(height: 70), // Agregue espacio en blanco debajo del texto
+                Form(
+                  key: formKey,
+                  child: Container(
+                    width: 350,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        TextFormField(
+                          cursorColor: Colors.brown,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            border: const OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.brown),
+                            ),
+                            focusedBorder: const OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.brown)
+                            ),
+
                             labelText: 'Correo electrónico',
                             labelStyle: GoogleFonts.quicksand(
                                 fontSize: 16,
-                                fontWeight: FontWeight.w700
-                            )
-                        ),
-                      ),
-                      SizedBox(height: 40),
-                      TextField(
-                        obscureText: true,
-                        decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: 'Contraseña',
-                            labelStyle: GoogleFonts.quicksand(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold
-                            )
-                        ),
-                      ),
+                                fontWeight: FontWeight.w700,
+                                color: Colors.brown
+                            ),
 
-                      SizedBox(height: 20),
-                      ElevatedButton(onPressed: (){print("Inicio de sesión pulsado");},
-                          child: Text(
-                              "LOGIN",
-                              style: GoogleFonts.quicksand(
+                          ),
+
+                          validator: (value){
+                            if (value == null || value.isEmpty == true) {
+                              setState(() {
+                                errorMailEmpty = true;
+                              });
+                              return 'Este campo es obligatorio';
+                            }
+                            else {
+                              if (!coincideMail.hasMatch(value)) {
+                                setState(() {
+                                  errorMailRegExp = true;
+                                });
+                                return 'por favor, sigue el formato user@domain.com';
+                              }
+                              else {
+                                return null;
+                              }
+                            }
+                          },
+                        ),
+                        SizedBox(height: 40), // Agregue espacio en blanco entre los campos de texto
+                        TextFormField(
+                          cursorColor: Colors.brown,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                              border: const OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.brown)
+                              ),
+                              focusedBorder: const OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.brown)
+                              ),
+                              labelText: 'Contraseña',
+                              labelStyle: GoogleFonts.quicksand(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.brown
+                              )
+                          ),
+                          validator: (value){
+                            if (value == null || value.isEmpty) {
+                              setState(() {
+                                errorPassword = true;
+                              });
+                              return 'Por favor, rellena el campo con tu contraseña.';
+                            }
+                            return null;
+                          },
+                        ),
+
+                        SizedBox(height: 20),
+                        ElevatedButton(
+                            onPressed: (){
+                              if (!formKey.currentState!.validate()) {
+                                return;
+                              }
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(content: Text('Cargando')),
+                              );
+
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.brown,
+                            ),
+                            child: Text(
+                                "LOGIN",
+                                style: GoogleFonts.quicksand(
                                   fontSize: 14.0,
                                   color: Colors.white,
                                   fontWeight: FontWeight.w900,
-                                  letterSpacing: 2.0
-                              )),
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.brown,
-                          )),
-                      SizedBox(height: 50),
-                      Text("¿No tienes cuenta?",
-                          style: GoogleFonts.quicksand(
+                                  letterSpacing: 2.0,
+
+                                ))),
+                        SizedBox(height: 50),
+                        Text("¿No tienes cuenta?",
+                            style: GoogleFonts.quicksand(
                               fontSize: 14.0,
                               color: Colors.black,
                               fontWeight: FontWeight.bold,
-                              letterSpacing: 2.0
-                          )
-                      ),
-                      SizedBox(height: 10,),
-                      ElevatedButton(onPressed: (){print("Registro pulsado");},
-                          child: Text(
-                              "REGISTRO",
-                              style: GoogleFonts.quicksand(
-                                  fontSize: 14.0,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w900,
-                                  letterSpacing: 2.0
-                              )),
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.brown,
-                          )),
+                              letterSpacing: 2.0,
 
-
-                    ],
+                            )
+                        ),
+                        SizedBox(height: 10,),
+                        ElevatedButton( onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.brown,
+                            ),
+                            child: Text(
+                                "REGISTRO",
+                                style: GoogleFonts.quicksand(
+                                    fontSize: 14.0,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w900,
+                                    letterSpacing: 2.0
+                                ))),
+                      ],
+                    ),
                   ),
                 ),
+
               ],
             ),
+
           ),
-
-
         ),
       ),
     );
@@ -131,4 +198,3 @@ class PetMateAppBar extends StatelessWidget implements PreferredSizeWidget {
   }
 
 }
-
