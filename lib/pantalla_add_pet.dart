82fec0f8d5f89cf0fd3269549a01b2bf5f49f1a4
Loann-ca <loann.cosano@gmail.com>
2023-04-22 13:36:00 +0200

    Todos los campos hechos

--- lib/pantalla_add_pet.dart
+++ lib/pantalla_add_pet.dart
@@ -12,7 +12,6 @@ class AddPet extends StatefulWidget {
 class AddPetState extends State<AddPet> {
   final formkey = GlobalKey<FormState>();
   bool NumberError = false;
-  bool DirectionError = false;
   bool dropdownError = false;
   String? dropdownValue;
   late String name;
@@ -182,7 +181,80 @@ class AddPetState extends State<AddPet> {
                                       .toList(),
                                   validator: (value) => value == null ? 'Este campo es requerido' : null,)
                             ),
-                            const SizedBox(height: 40),
+                            const SizedBox(height: 20),
+                            Container(
+                                padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+                                decoration: BoxDecoration(
+
+                                    border: Border.all(
+                                        color: dropdownError ? Colors.red : Colors.black),
+                                    borderRadius: BorderRadius.circular(5)
+                                ),
+
+                                child: DropdownButtonFormField<String>(
+                                  decoration: InputDecoration(
+                                    labelText: 'Tamaño',
+                                    labelStyle: TextStyle(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16,
+                                    ),
+                                    border: InputBorder.none,
+                                  ),
+                                  dropdownColor: Colors.brown,
+                                  onChanged: (String? changedValue) {
+                                    dropdownValue = changedValue!;
+                                    setState(() {
+                                      dropdownValue;
+                                    });
+                                  },
+                                  isExpanded: true,
+                                  style: GoogleFonts.quicksand(
+                                    color: Colors.black,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                  value: dropdownValue,
+                                  items: <String>["Pequeño", "Mediano", "Grande"]
+                                      .map((item) => DropdownMenuItem(
+                                    child: Text(item),
+                                    value: item,
+
+                                  ))
+                                      .toList(),
+                                  validator: (value) => value == null ? 'Este campo es requerido' : null,)
+                            ),
+                            const SizedBox(height: 20),
+                            TextFormField(
+                              decoration: InputDecoration(
+                                border: const OutlineInputBorder(
+                                    borderSide:
+                                    BorderSide(color: Colors.brown)),
+                                focusedBorder: const OutlineInputBorder(
+                                    borderSide:
+                                    BorderSide(color: Colors.brown)),
+                                labelText: "Año de nacimiento",
+                                labelStyle: GoogleFonts.quicksand(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w700,
+                                    color: NumberError
+                                        ? Colors.red
+                                        : Colors.black),
+                              ),
+                              cursorColor: Colors.brown,
+                              keyboardType: TextInputType.number,
+                              validator: (value) {
+                                final int? yearOfBirth = int.tryParse(value ?? '');
+                                if (yearOfBirth == null) {
+                                  return 'Este campo es requerido';
+                                }
+                                if (yearOfBirth < 1900 || yearOfBirth > DateTime.now().year) {
+                                  return 'Por favor, ingresa un año válido';
+                                }
+                                return null;
+                              },
+                            ),
+                            const SizedBox(height: 20),
                             TextFormField(
                                 decoration: InputDecoration(
                                   border: const OutlineInputBorder(
@@ -191,118 +263,41 @@ class AddPetState extends State<AddPet> {
                                   focusedBorder: const OutlineInputBorder(
                                       borderSide:
                                       BorderSide(color: Colors.brown)),
-                                  labelText: "Dirección de la protectora",
+                                  labelText: "Descripción",
                                   labelStyle: GoogleFonts.quicksand(
                                       fontSize: 16,
                                       fontWeight: FontWeight.w700,
-                                      color: DirectionError
+                                      color: NumberError
                                           ? Colors.red
                                           : Colors.black),
                                 ),
-                                cursorColor: Colors.black,
+                                cursorColor: Colors.brown,
+                                keyboardType: TextInputType.text,
                                 validator: (value) {
                                   if (value?.isEmpty ?? true) {
                                     setState(() {
-                                      DirectionError = true;
+                                      NumberError = true;
                                     });
                                     return 'Este campo es obligatorio';
+                                  } else if (!regExp.hasMatch(value!)) {
+                                    setState(() {
+                                      NumberError = true;
+                                    });
+                                    return 'Este campo requiere de letras';
                                   } else {
                                     setState(() {
-                                      DirectionError = false;
+                                      NumberError = false;
                                     });
-
+                                    return null;
                                   }
                                 }),
                             const SizedBox(height: 20),
                             ElevatedButton(
-                              onPressed: () async {
-                                if (formkey.currentState?.validate() ?? false ) {
-                                  var data = {
-
-                                  };
-                                  if(await sendRegisterRequest(data)) {
-                                    showDialog(
-                                        context: context,
-                                        builder: (BuildContext context) {
-                                          return AlertDialog(
-                                            icon: const Icon(Icons.pets_rounded),
-                                            title: const Text(
-                                                'Registro como protectora completado'),
-                                            titleTextStyle: GoogleFonts.quicksand(
-                                                fontSize: 16,
-                                                fontWeight: FontWeight.w700,
-                                                color: Colors.white),
-                                            backgroundColor:
-                                            const Color(0xFFC4A484),
-                                            shape: const RoundedRectangleBorder(
-                                                borderRadius: BorderRadius.all(
-                                                    Radius.circular(20))),
-                                            actions: <Widget>[
-                                              Column(
-                                                mainAxisAlignment:
-                                                MainAxisAlignment.start,
-                                                children: [
-                                                  ElevatedButton(
-                                                      style: ElevatedButton
-                                                          .styleFrom(
-                                                          backgroundColor:
-                                                          Colors.brown),
-                                                      onPressed: () {
-                                                        Navigator.of(context)
-                                                            .pop();
-                                                      },
-                                                      child: const Text('Cerrar'))
-                                                ],
-                                              )
-                                            ],
-                                          );
-                                        });
-                                  } else {
-                                    showDialog(
-                                        context: context,
-                                        builder: (BuildContext context) {
-                                          return AlertDialog(
-                                            icon: const Icon(Icons.pets_rounded),
-                                            title: const Text(
-                                                'Error en el registro'),
-                                            titleTextStyle: GoogleFonts.quicksand(
-                                                fontSize: 16,
-                                                fontWeight: FontWeight.w700,
-                                                color: Colors.white),
-                                            backgroundColor:
-                                            const Color(0xFFC4A484),
-                                            shape: const RoundedRectangleBorder(
-                                                borderRadius: BorderRadius.all(
-                                                    Radius.circular(20))),
-                                            actions: <Widget>[
-                                              Column(
-                                                mainAxisAlignment:
-                                                MainAxisAlignment.start,
-                                                children: [
-                                                  ElevatedButton(
-                                                      style: ElevatedButton
-                                                          .styleFrom(
-                                                          backgroundColor:
-                                                          Colors.brown),
-                                                      onPressed: () {
-                                                        Navigator.of(context)
-                                                            .pop();
-                                                      },
-                                                      child: const Text('Cerrar'))
-                                                ],
-                                              )
-                                            ],
-                                          );
-                                        });
-                                  }
-                                } else {
-                                  return;
-                                }
-                              },
+                              onPressed: (){},
                               style: ElevatedButton.styleFrom(
                                   backgroundColor: Colors.brown),
                               child: Text(
-                                'REGISTRARSE',
+                                'AÑADIR',
                                 style: GoogleFonts.quicksand(
                                     fontSize: 14,
                                     color: Colors.white,
